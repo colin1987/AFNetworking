39e7cf4338e521ff621f71f7533a8004808d9725
陈硕实 <bill@imhuihui.com>
2015-02-02 12:40:53 +0800

    添加网络请求前钩子

--- AFNetworking/AFHTTPRequestOperationManager.h
+++ AFNetworking/AFHTTPRequestOperationManager.h
@@ -304,7 +304,7 @@
                            success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
                            failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
 
-//modified by css
+//added by css
 - (void)executeOperation:(AFHTTPRequestOperation *)operation;
 
 @end
